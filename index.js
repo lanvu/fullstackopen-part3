4c45e007efa0554ddc17425f189092a4efbf1ed8
Lan Vu <vungoclancfc@gmail.com>
2020-08-09 13:37:27 -0500

    Finish 3.8

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+
+morgan.token('req-body', (req, res) => JSON.stringify(req.body))
+
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :req-body'
+  )
+)
 
 let persons = [
   {
