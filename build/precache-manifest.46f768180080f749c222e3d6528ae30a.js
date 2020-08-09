17423ccc424355d97bc0254d4024f6ac6e23a90b
Lan Vu <vungoclancfc@gmail.com>
2020-08-09 16:07:50 -0500

    Build UI

---
+++ build/precache-manifest.46f768180080f749c222e3d6528ae30a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223998ee20f56ef559506e423e448d26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "291f0997d677f0df6b3b",
+    "url": "/static/css/main.b757ef8f.chunk.css"
+  },
+  {
+    "revision": "fdc00f3d2f4001d77518",
+    "url": "/static/js/2.9d147208.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d147208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "291f0997d677f0df6b3b",
+    "url": "/static/js/main.8e1ef097.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
