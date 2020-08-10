89f06185aad50361d0b21698a746ec179f2eeb3a
Lan Vu <vungoclancfc@gmail.com>
2020-08-09 21:24:35 -0500

    Build UI

--- build/precache-manifest.ac0edbcde15c209f7a06c78120260ed3.js
+++ build/precache-manifest.ac0edbcde15c209f7a06c78120260ed3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "223998ee20f56ef559506e423e448d26",
+    "revision": "f761d27ba6f381d0d9eda40da6a72067",
     "url": "/index.html"
   },
   {
-    "revision": "291f0997d677f0df6b3b",
+    "revision": "558a84e634c3865a47c9",
     "url": "/static/css/main.b757ef8f.chunk.css"
   },
   {
-    "revision": "fdc00f3d2f4001d77518",
-    "url": "/static/js/2.9d147208.chunk.js"
+    "revision": "39355c7d523372253cb7",
+    "url": "/static/js/2.caeac873.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.9d147208.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.caeac873.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "291f0997d677f0df6b3b",
-    "url": "/static/js/main.8e1ef097.chunk.js"
+    "revision": "558a84e634c3865a47c9",
+    "url": "/static/js/main.4c63d37b.chunk.js"
   },
   {
     "revision": "9d2b4f7262278411ffe7",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46f768180080f749c222e3d6528ae30a.js"
+  "/precache-manifest.ac0edbcde15c209f7a06c78120260ed3.js"
 );
 
 self.addEventListener('message', (event) => {
